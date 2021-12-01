21d50922963e0023d6dfec177cb9a8d9711213ec
yoyoichi-ema <87355303+yoyoichi-ema@users.noreply.github.com>
2021-12-01 20:41:27 +0900

    テスト用にプロバイダーコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 import { Router } from "../src/router/Router";
+import { UserProvider } from "./providers/UserProvider";
 
 export default function App() {
   return (
-    <>
+    <UserProvider>
       <Router />
-    </>
+    </UserProvider>
   );
 }
